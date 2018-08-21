863677a6da36a60f3ba9bd6e6bacde770e83cc66
FernandoTorres5IM9 <42345621+FernandoTorres5IM9@users.noreply.github.com>
2018-08-20 20:34:36 -0500

    Add files via upload

---
+++ Practica 1/src/practica/pkg1/Practica1.java
@@ -0,0 +1,14 @@
+﻿
+package practica.pkg1;
+
+/**
+ *
+ * @author  Fernando Torres Pantoja 5IM9
+ */
+public class Practica1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo");
+    }
+    
+}
